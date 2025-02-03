7d96501bfd02b6f856a259c1a70341f08ea877a9
dangthimylinh <linhboure@gmail.com>
2025-02-03 20:46:41 +0700

    update from testcase01.java

--- testcase01.java
+++ testcase01.java
@@ -1,5 +1,8 @@
 public class testcase01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Update in testcase 01");
+        System.out.println("Update in testcase 01");
     }
 }
+
