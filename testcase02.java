34492aaea301097fcf1d6e932df4737c8b0ad825
dangthimylinh <linhboure@gmail.com>
2025-06-07 15:24:21 +0700

    update from branch hot fix

--- testcase02.java
+++ testcase02.java
@@ -2,5 +2,6 @@ public class testcase02 {
     public static void main(String[] args) {
         System.out.println("Git amend");
         System.out.println("hello");
+        System.out.println("edit from branch hot fix");
     }
 }
